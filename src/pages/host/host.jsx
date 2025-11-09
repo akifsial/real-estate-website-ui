f42cc2a2d7a5fef3e2e32a8642fccc4a3a034b92
akifsial <contact.akifsial@gmail.com>
2025-11-09 13:42:21 +0500

    fix importing

--- src/pages/host/host.jsx
+++ src/pages/host/host.jsx
@@ -1,47 +1,11 @@
-// import React from 'react'
-// import Header from './components/header'
-// import bannervideo from "../../assets/videos/video-banner.mp4"
-// import TopSection from './components/top-section'
 
-// function Host() {
-//     return (
-
-//         <div className="relative w-full h-screen overflow-hidden">
-//             {/* Background video */}
-//             <video
-//                 autoPlay
-//                 muted
-//                 loop
-//                 className="absolute top-1/2 left-1/2 w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 z-0"
-//             >
-//                 <source src={bannervideo} type="video/mp4" />
-//             </video>
-
-//             {/* Dull overlay */}
-//             <div className="absolute inset-0 bg-black/60 z-10"></div>
-
-//             {/* Header */}
-//             <div className="relative lg:block hidden z-20">
-//                 <Header />
-//             </div>
-
-//             {/* Top section content */}
-//             <div className="absolute top-[35%] z-20 w-full">
-//                 <TopSection />
-//             </div>
-//         </div>
-
-//     )
-// }
-
-// export default Host
 
 import React from "react";
 import { motion } from "framer-motion";
 import Header from "./components/header";
 import bannervideo from "../../assets/videos/video-banner.mp4";
 import TopSection from "./components/top-section";
-import HowItWorks from "./components/HOW-IT-WORKS.JSX";
+import HowItWorks from "./components/how-it-works";
 import WhyChoose from "./components/why-choose";
 import Footer from "./components/footer";
 
